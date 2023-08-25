fc80ebf9b0447677e0b302e0e406b254d574833d
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-08-25 09:09:44 +0300

    Create 3.Supermarket.py

---
+++ ListsasStacksandQueues/3.Supermarket.py
@@ -0,0 +1,14 @@
+from collections import deque
+q = deque()
+
+while True:
+    command = input()
+    if command == "Paid":
+        while q:
+            print(q.popleft())
+    if command == "End":
+        print(f"{len(q)} people remaining.")
+        break
+    if command == "Paid":
+        continue
+    q.append(command)
