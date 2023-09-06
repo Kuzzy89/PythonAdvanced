aa4883e057992de6845aef1bd10088dfb2dd3397
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-09-06 08:41:50 +0300

    Create 6.Symbol in Matrix.py

---
+++ Multidementional_List/6.Symbol in Matrix.py
@@ -0,0 +1,20 @@
+def find_symbol(matrix, symbol):
+
+    for row_index in range(n):
+        for col_index in range(n):
+            if matrix[row_index][col_index] == symbol:
+                return row_index, col_index
+    return None
+
+
+n = int(input())
+matrix = [list(input()) for _ in range(n)]
+symbol = input()
+result = find_symbol(matrix, symbol)
+
+if result:
+    row_index, col_index = result
+    print(f"({row_index}, {col_index})")
+else:
+    print(f"{symbol} does not occur in the matrix")
+
