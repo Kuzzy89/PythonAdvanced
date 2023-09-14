0a01d0f554c622ec00095481ae16a5e6e07225e8
Stanislav Andreev Dimitrov <115157844+Kuzzy89@users.noreply.github.com>
2023-09-14 07:00:31 +0300

    Create 33.py

---
+++ ex/33.py
@@ -0,0 +1,16 @@
+def gather_credits(credit, *args):
+
+    courses = []
+    credits = 0
+
+    for i in range(len(args)):
+        object = args[i].
+
+    if credit > credits:
+        return f"You need to enroll in more courses! You have to gather {credit - credits} credits more."
+
+
+print(gather_credits(
+    80,
+    ("Basics", 27),
+))
